4f40a1abfcabfc1e60e7140f861f049ad7e0c906
cauvmou <cauvmou.8855@gmail.com>
2024-02-14 01:26:52 +0100

    added basic argument parsing

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod lexer;
 mod exec;
 mod compile;
 
+use clap::Parser;
 use crate::exec::interpret_tokens;
 use crate::lexer::tokenize;
 
@@ -16,8 +17,29 @@ enum SyscallArgType {
     CellPointer,
 }
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value = "Warn", value_names = ["Trace", "Debug", "Info", "Warn", "Error"], help = "Verbosity of the interpreter.")]
+    log_level: log::Level,
+    #[arg(short, long, default_value = "false", help = "If the inline flag is set, then the program will be treated as the code to be executed, else it is assumed to be a path to a systemf source code file.")]
+    inline: bool,
+    #[arg(last = true, help = "A path to a file or systemf code, depending on the inline flag.")]
+    program: String,
+}
+
+struct Group {
+    path: Option<String>,
+    program: Option<String>,
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    simple_logger::init()?;
-    let tokens = tokenize(&std::fs::read_to_string(std::env::args().collect::<Vec<String>>().last().expect("No program was supplied!")).expect("Failed to read program!")).unwrap();
+    let args = Args::parse();
+    simple_logger::init_with_level(args.log_level)?;
+    let tokens = if args.inline {
+        tokenize(&args.program)?
+    } else {
+        tokenize(&std::fs::read_to_string(args.program)?)?
+    };
     interpret_tokens(&tokens)
 }
